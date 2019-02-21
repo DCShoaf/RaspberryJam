c6153ae4e5bb67c226f30c3260a1f8051df40113
DESKTOP-PA9S084\Donald Shoaf <donaldshoaf@yahoo.com>
2019-02-21 12:00:24 -0500

    Commented out quantization as a future feature. Added note length field to SampleProject. Now plays one note per beat.

--- Sound.py
+++ Sound.py
@@ -3,22 +3,27 @@ import time
 
 
 class SampleProject:
-    T = 2.0/3.0
-    none = 0
-    whole = 4
-    half = 2
-    quarter = 1
-    eight = .5
-    eightT = T * eight
-    sixteen = .25
-    sixteenT = T * sixteen
-    thirtyTwo = .125
-    thirtyTwoT = thirtyTwo * T
+    # T = 2.0/3.0
+    # none = 0
+    # whole = 4
+    # half = 2
+    # quarter = 1
+    # eight = .5
+    # eightT = T * eight
+    # sixteen = .25
+    # sixteenT = T * sixteen
+    # thirtyTwo = .125
+    # thirtyTwoT = thirtyTwo * T
 
     def __init__(self, bpm):
         self.bpm = bpm
+        self.noteLength = (1.0 / (4 * 60)) * bpm
         self.stepLength = 64
-        self.quant = 1
+        self.sampleArray = [-1] * 64
+
+    def addsample(self, sampleNumber, index):
+        self.sampleArray[index] = sampleNumber
+        # self.quant = 1
         # 0 = no quantization TODO
         # 1 = whole
         # 2 = half
@@ -31,15 +36,26 @@ class SampleProject:
         # 63 = thirty second triplet
 
 
-sound = Sound('C:\\Users\\Donald Shoaf\\Desktop\\SAMPLER\\TEST_AUDIO_PCM.wav')
-# pygame.init()
 pygame.mixer.init()
 soundb = pygame.mixer.Sound('C:\\Users\\Donald Shoaf\\Desktop\\SAMPLER\\TEST_AUDIO_PCM.wav')
 sounda = pygame.mixer.Sound('C:\\Users\\Donald Shoaf\\Desktop\\SAMPLER\\BASS_AUDIO.wav')
+masterArray = [sounda, soundb]
 channel1 = pygame.mixer.Channel(1)
-channel1.play(soundb)
-sounda.play()
-while channel1.get_busy():
-    a = 0
-channel1.play(soundb)
-time.sleep(5)
+channel2 = pygame.mixer.Channel(2)
+channelArray = [channel1, channel2]
+
+project = SampleProject(60)
+project.addsample(0, 0)
+project.addsample(1, 4)
+project.addsample(1, 8)
+print(project.noteLength)
+for sampleNumber in project.sampleArray:
+    if sampleNumber != -1:
+        channelArray[sampleNumber].play(masterArray[sampleNumber])
+    time.sleep(project.noteLength)
+
+# channel1.play(soundb)
+# sounda.play()
+
+# channel1.play(soundb)
+# time.sleep(5)
